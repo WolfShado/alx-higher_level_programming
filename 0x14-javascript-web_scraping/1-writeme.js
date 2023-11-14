845f70dc341bad9fe937eb81def2876705401234
WolfShado <chamess.souib@gmail.com>
2023-11-14 14:57:40 -0800

    finishing

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const [fileName, text] = process.argv.slice(2);
+fs.writeFile(fileName, text, 'utf8', (err) => {
+  if (err) console.log(err.message);
+});
